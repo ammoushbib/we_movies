7340b218457327001084cfd3c640302139eb72db
Habib Ammous <habib.ammous@sofrecom.com>
2024-10-23 17:35:03 +0100

    [in progress] listing from third party api + in progress modal

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\MovieService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    public function __construct(private MovieService $movieService)
+    {
+        $this->movieService = $movieService;
+    }
+    #[Route('/', name: 'app_home')]
+    public function index(Request $request): Response
+    {
+        $genres = $this->movieService->getGenres();
+        $movies = $this->movieService->getTopRatedMovies()['results'];
+
+        $bestRatedMovie = array_shift($movies);
+
+
+        return $this->render('home/index.html.twig', [
+            'genres' => $genres['genres'],
+            'movies' => $movies,
+            'best_rated_movie' => $bestRatedMovie
+        ]);
+    }
+
+    #[Route('/search', name: 'search_movies')]
+    public function search(Request $request): JsonResponse
+    {
+        $query = $request->query->get('query');
+        $movies = $this->movieService->searchMovies($query);
+
+        return new JsonResponse($movies);
+    }
+}
---
+++ src/Controller/MovieController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\MovieService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MovieController extends AbstractController
+{
+    public function __construct(MovieService $movieService)
+    {
+        $this->movieService = $movieService;
+    }
+
+    #[Route('/movie', name: 'app_movie')]
+    public function index(): Response
+    {
+        $movies = $this->movieService->getPopularMovies();
+
+        return $this->render('movie/index.html.twig', [
+            'movies' => $movies['results'],
+        ]);
+    }
+
+
+    #[Route('/movie/{id}', name: 'movie_plays')]
+    public function play(int $id): Response
+    {
+        $movie = $this->movieService->getMovieDetails($id);
+        $videos = $this->movieService->getMovieVideos($id);
+
+        $trailer = array_filter($videos['results'], function($video) {
+            return $video['type'] === 'Trailer';
+        });
+
+        $trailer = reset($trailer);
+        if ($trailer) {
+            $trailer['url'] = 'https://www.youtube.com/watch?v=' . $trailer['key'];
+        }
+
+        return $this->render('movie/play.html.twig', [
+            'movie' => $movie,
+            'trailer' => $trailer
+        ]);
+    }
+    #[Route('/movies/{id}', name: 'movie_play')]
+    public function plays(int $id): Response
+    {
+        $movie = $this->movieService->getMovieDetails($id);
+
+        return new JsonResponse($movie);
+    }
+}
